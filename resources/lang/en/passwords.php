4cb3081c1c0d74400445e542e3e9a751e7e7f1cc
henriquetroiano <henriquetroiano@hotmail.com>
2020-12-16 23:32:38 -0300

    post logout working

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Sua senha foi alterada.',
+    'sent' => 'Enviamos um email com o link para redefinição de senha. Verifique sua caixa de Spam.',
+    'throttled' => 'Muitas tentativas. Aguarde um tempo antes de tentar novamente.',
+    'token' => 'O link de alteração de senha expirou ou é inválido.',
+    'user' => "Não encontramos este endereço de e-mail.",
 
 ];

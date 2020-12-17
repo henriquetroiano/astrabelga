4bbc424985bf8772cc6015c31bd268b6685285b4
henriquetroiano <henriquetroiano@hotmail.com>
2020-12-16 21:35:57 -0300

    post logout working

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -44,7 +44,6 @@
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                         @csrf
                     </form>
-                    <a class="dropdown-item" href="{{ url('/logout') }}">Sair</a>
                 </div>
             </div>
         @else

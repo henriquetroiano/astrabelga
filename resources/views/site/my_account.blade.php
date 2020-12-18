66bb6bdc78398e2697083599b1468e4b1eb4d559
henriquetroiano <henriquetroiano@hotmail.com>
2020-12-17 22:57:07 -0300

    catalogos

--- resources/views/site/my_account.blade.php
+++ resources/views/site/my_account.blade.php
@@ -5,11 +5,64 @@
 @section('content')
 
 
-    <div class="container catalogue pt-3">
+    <div class="container account pt-3">
 
         @component('components.ads.horizontal')@endcomponent
 
-        
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Meus dados</div>
+    
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+    
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">Nome</label>
+    
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ Auth::user()->name }}" required autocomplete="name" autofocus>
+    
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+    
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">Endereço de e-mail</label>
+    
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
+    
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+    
+                            
+    
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Alterar dados
+                                    </button>
+                                    <a href="{{ url('/') }}" type="button" class="btn btn-secondary">
+                                        Voltar
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         
     </div>
 @endsection

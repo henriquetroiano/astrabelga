38216976d9445fb6334dfe14898d6cc2a71d191e
henriquetroiano <henriquetroiano@hotmail.com>
2020-12-21 23:03:26 -0300

    filter

--- resources/views/site/catalogos.blade.php
+++ resources/views/site/catalogos.blade.php
@@ -12,37 +12,44 @@
         <h4>Catálogo de Peças Compatíveis / Intercambiáveis</h4>
         <span>Não recomendamos peças paralelas (sem marca). <strong>Evite</strong> o uso de tais peças.</span>
 
-        <form action="{{ url('/filter-catalogue') }}" method="POST" id="form-catalogo">
-            <div class="form-row align-items-center">
-              <div class="col-auto my-1">
-                <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preferência</label>
-                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                    <option selected disabled>Selecione o motor</option>
-                    <option value="2.0">Motor 2.0</option>
-                    <option value="1.8">Motor 1.8</option>
-                </select>
-              </div>
-              <div class="col-auto my-1">
-                <div class="custom-checkbox mr-sm-2">
-                    <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preferência</label>
-                    <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                        <option selected disabled>Selecione uma categoria</option>
-                        <option value="eletrica">Elétrica</option>
-                        <option value="mecanica">Mecânica</option>
-                        <option value="acabamento">Acabamento Interno ou Externo</option>
-                        <option value="lataria">Lataria</option>
-                    </select>
+    
+          <button class="btn btn-primary filter" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            Filtros +
+          </button>
+        <div class="collapse mb-3" id="collapseExample">
+          <div class="card card-body">
+            <form action="{{ url('/filter-catalogue') }}" method="POST" id="form-catalogo">
+              <div class="container-items">
+                <div class="my-1">
+                  <label class="sr-only" for="inlineFormCustomSelect">Preferência</label>
+                  <select class="custom-select" id="inlineFormCustomSelect">
+                      <option selected disabled>Selecione o motor</option>
+                      <option value="2.0">Motor 2.0</option>
+                      <option value="1.8">Motor 1.8</option>
+                  </select>
                 </div>
+                <div class="my-1">
+                  <div class="custom-checkbox">
+                      <label class=" sr-only" for="inlineFormCustomSelect">Preferência</label>
+                      <select class="custom-select " id="inlineFormCustomSelect">
+                          <option selected disabled>Selecione uma categoria</option>
+                          <option value="eletrica">Elétrica</option>
+                          <option value="mecanica">Mecânica</option>
+                          <option value="acabamento">Acabamento Interno ou Externo</option>
+                          <option value="lataria">Lataria</option>
+                      </select>
+                  </div>
+                </div>
+                  <button type="submit" class="btn btn-primary my-1">Filtrar</button>
               </div>
-              <div class="col-auto my-1">
-                <button type="submit" class="btn btn-primary">Filtrar</button>
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
+        </div>
+        
 
         
         <div class="content">
-            <h5>Resultados:</h5>
+            <h3>Resultados:</h3>
             <table class="table table-bordered table-hover">
                 <thead class="thead-dark">
                   <tr>

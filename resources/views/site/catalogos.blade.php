5775ed1ee38d9ee9940dc587e15720036258bbc0
unknown <analitico01@gmail.com>
2021-01-13 15:36:00 -0300

    changes on layout in general

--- resources/views/site/catalogos.blade.php
+++ resources/views/site/catalogos.blade.php
@@ -17,7 +17,7 @@
 					<h4>Nenhuma Peça Cadastrada</h4>
 				@endif
 				@foreach ($catalogos as $peca)
-					<div class="card item-catalogue-client">
+					<div class="card item-catalogue-client mb-3">
 						<div class="card-header">
 							<h4 class="mb-0 mr-2">{{$peca->name}}</h4> 
 							<a class="btn btn-success" href="{{ url('/catalogo' . '/'. $peca->id) }}">Ver Detalhes</a>
